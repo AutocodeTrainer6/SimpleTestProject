88c97394bd41eb594f81a1eae70bdfef7e40e5f7
Khasankhon Artikov <Khasankhon_Artikov@epam.com>
2022-07-16 16:57:14 +0500

    initial

---
+++ src/test/java/com/google/TestSimple.java
@@ -0,0 +1,29 @@
+package com.google;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class TestSimple {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+    }
+
+    @Test
+    public void simpleTest(){
+        driver.navigate().to("https://google.com");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.close();
+        driver = null;
+    }
+}

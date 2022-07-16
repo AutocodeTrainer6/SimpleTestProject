b03389290ef8ac0da69a30d5a9b00f6ffc8a2267
AutocodeTrainer6 <109236753+AutocodeTrainer6@users.noreply.github.com>
2022-07-16 21:00:59 +0500

    Update TestSimple.java
    tests were commented

--- src/test/java/com/google/TestSimple.java
+++ src/test/java/com/google/TestSimple.java
@@ -10,20 +10,20 @@ import org.testng.annotations.Test;
 public class TestSimple {
     private WebDriver driver;
 
-    @BeforeTest
-    public void setUp(){
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
-    }
+//     @BeforeTest
+//     public void setUp(){
+//         WebDriverManager.edgedriver().setup();
+//         driver = new EdgeDriver();
+//     }
 
-    @Test
-    public void simpleTest(){
-        driver.navigate().to("https://google.com");
-    }
+//     @Test
+//     public void simpleTest(){
+//         driver.navigate().to("https://google.com");
+//     }
 
-    @AfterClass
-    public void tearDown(){
-        driver.close();
-        driver = null;
-    }
+//     @AfterClass
+//     public void tearDown(){
+//         driver.close();
+//         driver = null;
+//     }
 }

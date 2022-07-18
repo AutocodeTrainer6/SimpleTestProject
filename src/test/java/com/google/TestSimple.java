ff7a9ea84bbb05081fd29f86cff7e69b73893db8
AutocodeTrainer6 <109236753+AutocodeTrainer6@users.noreply.github.com>
2022-07-18 10:20:29 +0500

    Update TestSimple.java
    add import Assert

--- src/test/java/com/google/TestSimple.java
+++ src/test/java/com/google/TestSimple.java
@@ -1,9 +1,5 @@
 package com.google;
-import org.openqa.selenium.WebDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
